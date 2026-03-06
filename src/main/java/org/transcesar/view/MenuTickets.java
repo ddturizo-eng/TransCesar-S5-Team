71460bcc31951f00ad63f404dd91b3d76f449cc2
ddturizo-eng <ddturizo@unicesar.edu.co>
2026-03-06 11:08:02 -0500

    feat: add MenuTickets con flujo de venta de tiquete

---
+++ src/main/java/org/transcesar/view/MenuTickets.java
@@ -0,0 +1,15 @@
+package org.transcesar.view;
+import java.util.Scanner;
+public class MenuTickets {
+
+    private  Scanner sc = new Scanner(System.in);
+
+            public void  mostrar(){
+
+                System.out.println(" Gestion de tickets");
+                System.out.println("1. Registrar Ticket");
+                System.out.println("2. Listar tickets");
+                System.out.println("0. Volver");
+
+            }
+}
