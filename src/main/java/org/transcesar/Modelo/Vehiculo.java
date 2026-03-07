658c9fd0d709e002cbc6f75bd7ce6294f2dc2059
daniel florez <dricardoflorez@unicesar.edu.co>
2026-03-06 19:06:25 -0500

    feat: add clase abstracta Vehiculo con atributos y encapsulamiento

---
+++ src/main/java/org/transcesar/Modelo/Vehiculo.java
@@ -0,0 +1,58 @@
+package org.transcesar.Modelo;
+
+public abstract class Vehiculo implements Imprimible {
+
+    private String placa;
+    private String ruta;
+    private int capacidadMaxima;
+    private int pasajerosActuales;
+    private double tarifaBase;
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+        this.tarifaBase = tarifaBase;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getPasajerosActuales() {
+        return pasajerosActuales;
+    }
+
+    public void setPasajerosActuales(int pasajerosActuales) {
+        this.pasajerosActuales = pasajerosActuales;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public void setTarifaBase(double tarifaBase) {
+        this.tarifaBase = tarifaBase;
+    }
+}
